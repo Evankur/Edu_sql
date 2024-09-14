e2284d6d5c7ee8727141ded178f8dd5ab3c93f0a
evan <evankurniawan76@Gmail.com>
2024-09-14 23:16:29 +0700

    sqlsyntax

---
+++ Script-1.sql
@@ -0,0 +1,16 @@
+insert into user_edu values (1, "Vannn", "van@gmail.com", "08542332", "laki", "jln.senang" );
+insert into user_edu values (2, "Abby", "aby@gmail.com", "08545552", "perempuan", "jln.senang" );
+insert into user_edu values (3, "Zafira", "Zafir@gmail.com", "08545909", "perempuan", "jln.senang" );
+insert into user_edu values (4, "Sulaiman", "Sulaiman@hotmail.com", "08511223", "laki", "jln.senang" );
+insert into user_edu values (5, "Ottoman", "Ottom@hotmail.com", "08543232", "laki", "jln.senang" );
+insert into user_edu values (6, "Williaam", "Will@gmail.com", "08549564", "laki", "jln.kesempitan" );
+insert into user_edu values (7, "Budi", "budi@gmail.com", "08215111", "laki", "jln.kesempitan" );
+insert into user_edu values (8, "Sonia", "son@gmail.com", "08500091", "perempuan", "jln.bulanangkasa" );
+insert into user_edu values (9, "Terry", "Terry@hotmail.com", "0823299", "laki", "jln.induk" );
+insert into user_edu values (10, "Abuba", "abuba@hotmail.com", "0897531", "laki", "jln.semangka" );
+insert into user_edu values (12, "Irfan", "fan@hotmail.com", "0897191", "laki", "jln.sehatan" );
+insert into user_edu values (13, "Yunna", "Yunnyun@hotmail.com", "0895223", "perempuan", "jln.buah" );
+insert into user_edu values (14, "Umika", "Umi@hotmail.com", "08901098", "perempuan", "jln.semangka" );
+insert into user_edu values (15, "Askan", "Aska@gmail.com", "08656001", "laki", "jln.skesempitan" );
+
+select name as name_person, phone as phone_person, address as address_person from user_edu ;
---
+++ export_data.sql
@@ -0,0 +1,18 @@
+INSERT INTO db_user.user_edu (user_id,name,email,phone,gender,address) VALUES
+	 (1,'Vannn','van@gmail.com',8542332,'laki','jln.senang'),
+	 (2,'Abby','aby@gmail.com',8545552,'perempuan','jln.senang'),
+	 (2,'Abby','aby@gmail.com',8545552,'perempuan','jln.senang'),
+	 (2,'Abby','aby@gmail.com',8545552,'perempuan','jln.senang'),
+	 (3,'Zafira','Zafir@gmail.com',8545909,'perempuan','jln.senang'),
+	 (4,'Sulaiman','Sulaiman@hotmail.com',8511223,'laki','jln.senang'),
+	 (5,'Ottoman','Ottom@hotmail.com',8543232,'laki','jln.senang'),
+	 (6,'Williaam','Will@gmail.com',8549564,'laki','jln.kesempitan'),
+	 (7,'Budi','budi@gmail.com',8215111,'laki','jln.kesempitan'),
+	 (8,'Sonia','son@gmail.com',8500091,'perempuan','jln.bulanangkasa');
+INSERT INTO db_user.user_edu (user_id,name,email,phone,gender,address) VALUES
+	 (9,'Terry','Terry@hotmail.com',823299,'laki','jln.induk'),
+	 (10,'Abuba','abuba@hotmail.com',897531,'laki','jln.semangka'),
+	 (12,'Irfan','fan@hotmail.com',897191,'laki','jln.sehatan'),
+	 (13,'Yunna','Yunnyun@hotmail.com',895223,'perempuan','jln.buah'),
+	 (14,'Umika','Umi@hotmail.com',8901098,'perempuan','jln.semangka'),
+	 (15,'Askan','Aska@gmail.com',8656001,'laki','jln.skesempitan');
